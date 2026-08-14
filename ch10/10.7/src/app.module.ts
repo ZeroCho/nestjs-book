996162aaf99807977fa2ec27ab51e9a623cdf66e
Hyunyoung Cho <zerohch0@gmail.com>
2026-08-14 18:11:04 +0900

    Update app.module.ts

--- ch10/10.7/src/app.module.ts
+++ ch10/10.7/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import configuration from './config/configuration';
       load: [configuration],
       isGlobal: true,
     }),
+    ChatModule,
   ],
   controllers: [AppController],
   providers: [AppService],
